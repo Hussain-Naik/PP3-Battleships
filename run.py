9db144fc1d24d5b864f60afd5c5bf91d58e40d92
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-08-01 09:43:09 +0100

    Add main function with terminal menu

--- run.py
+++ run.py
@@ -2,6 +2,25 @@ from simple_term_menu import TerminalMenu
 
 
 # Menu option lists
-option_main = ['Start', 'Exit']
+options_main = ['Start', 'Exit']
 # Menu Variables
-main_menu = TerminalMenu(option_main,title='Main Menu')
+main_menu = TerminalMenu(options_main,title='Main Menu')
+
+def main():
+    """
+    Main Function.
+    """
+    game_running = True
+
+    while game_running:
+        user_choice = main_menu.show()
+        if options_main[user_choice] == "Start":
+            print('Starting Game...')
+        elif options_main[user_choice] == "Exit":
+            print('Closing Game...')
+            game_running = False
+    
+# Checking if we are running this file directly.
+if __name__ == "__main__":
+    # Calling the main function.
+    main()
