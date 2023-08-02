a42268c29b9a7ecf27f73102e0353a075138b47c
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-08-02 09:11:00 +0100

    Add basic grid display

--- run.py
+++ run.py
@@ -7,16 +7,25 @@ options_main = ['Start', 'Exit']
 # Menu Variables
 main_menu = TerminalMenu(options_main,title='Main Menu')
 
+def generate_grid():
+    """
+    Generates and returns a grid of nodes as a 2D numpy array.
+    """
+    #return np.array([[Node(row, col) for col in range(WIDTH)] for row in range(HEIGHT)])
+    return np.arange(100).reshape(10,10)
+
 def main():
     """
     Main Function.
     """
     game_running = True
-
+    grid = generate_grid()
+    
     while game_running:
         user_choice = main_menu.show()
         if options_main[user_choice] == "Start":
             print('Starting Game...')
+            print(grid)
         elif options_main[user_choice] == "Exit":
             print('Closing Game...')
             game_running = False
