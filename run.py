05321148503abb81f3398de9bac09855aab52bb8
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-08-02 19:02:06 +0100

    Add grid display render

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 from time import sleep
 
-terminal = Terminal()
+term = Terminal()
 # Menu option lists
 options_main = ['Start', 'Exit']
 # Menu Variables
@@ -25,7 +25,16 @@ def output_string(string):
         sys.stdout.write(char)
         sys.stdout.flush()
 
-
+def display_grid(grid):
+    """
+    Displays the grid in the terminal.
+    """
+    with term.hidden_cursor():
+        print(term.home + term.clear)
+        for row in grid:
+            grid_string = " ".join(str('-') for col in row) +'\n'
+            output_string(grid_string)
+            sleep(0.05)
 
 def main():
     """
@@ -34,9 +43,12 @@ def main():
     game_running = True
     board = generate_grid()
     while game_running:
-        user_choice = main_menu.show()
+        display_grid(board)
+        #user_choice = main_menu.show()
         if options_main[user_choice] == "Start":
+            print(term.home + term.clear)
             output_string('Starting Battleships Game...\n')
+            
         elif options_main[user_choice] == "Exit":
             output_string('Closing Battleships Game...\n')
             game_running = False
