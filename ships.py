6ea8ce50978108d99e9a8fd286f7b75a744233fe
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-08-04 21:59:58 +0100

    Add board nodes to ship method

--- ships.py
+++ ships.py
@@ -9,10 +9,11 @@ class Ship:
     vertical_size = 1
     horizontal_size = 1
     nodes = []
+    
     def __init__(self, size):
         self.size = size
         self.horizontal_size = size
-        self.nodes = np.array([Node(row, 0) for row in range(size)])
+        self.nodes = [Node(row, 0) for row in range(size)]
         #self.nodes = np.array([Node(0,0), Node(0,1), Node(0,2)])
         self.sunk = all([node.used for node in self.nodes])
 
@@ -31,3 +32,14 @@ class Ship:
     
     def update_status(self):
         self.sunk = all([node.used for node in self.nodes])
+
+    def assign_ship_to_board(self, board):
+        placed_nodes = []
+        for node in self.nodes:
+            placed_nodes.append(board[node.col][node.row])
+            print(id(board[node.col][node.row]))
+            print(id(node))
+        self.nodes = placed_nodes
+    def confirm_placement(self):
+        for node in self.nodes:
+            node.occupy()
