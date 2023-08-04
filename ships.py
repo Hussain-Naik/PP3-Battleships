72e83eca36689bd454f2948a88446de7f86cc383
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-08-04 10:11:36 +0100

    Add ship rotate method

--- ships.py
+++ ships.py
@@ -1,7 +1,30 @@
+import numpy as np
+from node import Node
 class Ship:
     """
     Battleship class
     """
-    def __init__(self, node, size):
-        self.node = node
-        self.size = size
+    sunk = False
+    vertical = True
+    nodes = []
+    def __init__(self, size):
+        #self.nodes = np.array([[Node(row, col) for col in range(0)] for row in range(size)])
+        self.nodes = np.array([Node(0,0), Node(0,1), Node(0,2)])
+
+    def print_location(self):
+        return self.nodes
+    
+    def rotate_ship(self):
+        for node in self.nodes:
+            temp_row = node.return_row()
+            temp_col = node.return_col()
+            node.col = temp_row
+            node.row = temp_col
+        self.vertical = False if True else False
+battleship = Ship(3)
+for node in battleship.nodes:
+    print((node.location()))
+
+battleship.rotate_ship()
+for node in battleship.nodes:
+    print((node.location()))
