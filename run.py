067b77949ef16cd1cb129b787828bd253ebc7f88
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-08-05 17:27:56 +0100

    Add test for ship node sets

--- run.py
+++ run.py
@@ -109,7 +109,7 @@ def main():
     b1.assign_ship_to_board(board)
     board[0,0].occupy()
     b1.confirm_placement()
-    print(b2.return_node_set)
+    print(b2.return_node_set())
     while True:
         user_choice = main_menu.show()
         if options_main[user_choice] == "Start":
