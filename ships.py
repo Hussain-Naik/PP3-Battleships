a1ed8e2e658f550c9ab7a4bab742e1bcc59b858e
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-08-11 09:41:42 +0100

    Add pep8 validation

--- ships.py
+++ ships.py
@@ -1,5 +1,7 @@
 import random
 from node import Node
+
+
 class Ship:
     """
     Battleship class
@@ -10,26 +12,26 @@ class Ship:
     horizontal_size = 1
     nodes = []
     name = ""
-    
+
     def __init__(self, size):
-        #Size of the ship
+        # Size of the ship
         self.size = size
-        #Set horizontal size for checking end collision
+        # Set horizontal size for checking end collision
         self.vertical_size = size
-        #Set temporary nodes for ship
+        # Set temporary nodes for ship
         self.nodes = [Node(0, x) for x in range(size)]
-        #Set ship sunk boolean based of all node used variable
+        # Set ship sunk boolean based of all node used variable
         self.sunk = all([node.used for node in self.nodes])
         self.name = self.name_ship()
 
     def __str__(self):
         # override str method to display ship type as string.
         if self.sunk:
-            return ("\u0336".join(self.name) + "\u0336" 
+            return ("\u0336".join(self.name) + "\u0336"
                     + " " * (11 - len(self.name)))
         else:
             return self.name + " " * (11 - len(self.name))
-    
+
     def name_ship(self):
         if len(self.nodes) == 5:
             return "Carrier"
@@ -41,14 +43,15 @@ class Ship:
             return "Submarine"
         else:
             return "Patrol Boat"
+
     def print_location(self):
         return self.nodes
-    
+
     def display_sunk_ship(self):
         """
         Method to set ship nodes to first letter of ship name
         """
-        if self.sunk == True:
+        if self.sunk is True:
             for node in self.nodes:
                 node.name = self.name[0]
 
@@ -57,24 +60,24 @@ class Ship:
         Rotate method for ship
         """
         for node in self.nodes[1:]:
-            #Assign temporary variables
+            # Assign temporary variables
             temp_row = node.return_row()
             temp_col = node.return_col()
-            #Invert row and col
+            # Invert row and col
             node.col = temp_row
             node.row = temp_col
-        
+
         self.change_vertical_status()
 
     def change_vertical_status(self):
         """
         Method to change vertical status
         """
-        #Change vertical status
+        # Change vertical status
         self.vertical = not self.vertical
-        #store ship current sizes as temp
+        # store ship current sizes as temp
         (temp_horizontal, temp_vertical) = self.return_size()
-        #Switch Vertical and horizontal sizes
+        # Switch Vertical and horizontal sizes
         self.vertical_size = temp_horizontal + 1
         self.horizontal_size = temp_vertical + 1
 
@@ -87,13 +90,13 @@ class Ship:
         col = self.nodes[0].return_col()
         row = self.nodes[0].return_row()
         (col_size, row_size) = self.return_size()
-        for x,node in enumerate(self.nodes[1:]):
+        for x, node in enumerate(self.nodes[1:]):
             if col_size < row_size:
                 new_list.append(Node(row + x + 1, col))
             else:
                 new_list.append(Node(row, col + x + 1))
         return new_list
-    
+
     def update_status(self):
         """
         Update ship sunk status
@@ -104,16 +107,16 @@ class Ship:
     def assign_ship_to_board(self, board):
         """
         Add board nodes to ship so ship
-        nodes reference same board node 
+        nodes reference same board node
         """
-        #Temporary list
+        # Temporary list
         placed_nodes = []
 
-        #loop through all nodes
+        # loop through all nodes
         for node in self.nodes:
-            #Add board node with node col and row to temp list
+            # Add board node with node col and row to temp list
             placed_nodes.append(board[node.col][node.row])
-        #Save node list with temporary list
+        # Save node list with temporary list
         self.nodes = placed_nodes
 
     def is_ship_placed(self):
@@ -121,39 +124,39 @@ class Ship:
         Method to return if ship has been placed on board
         """
         return all([node.occupied for node in self.nodes])
-    
+
     def confirm_placement(self):
         """
         Set all ship nodes to occupied variable to True
         """
-        #Loop through all ship nodes
+        # Loop through all ship nodes
         for node in self.nodes:
-            #Call node occupy method
+            # Call node occupy method
             node.occupy()
 
     def assign_to_player(self):
         """
         Set all ship nodes to occupied variable to True
         """
-        #Loop through all ship nodes
+        # Loop through all ship nodes
         for node in self.nodes:
-            #Call node occupy method
+            # Call node occupy method
             node.set_players()
-    
+
     def return_node_set(self):
         node_set = set()
         for node in self.nodes:
             node_set.add(node.location())
-        
+
         return node_set
 
     def set_random_position(self):
         self.reset_ship()
-        random_rotate = random.randint(0,1)
+        random_rotate = random.randint(0, 1)
         if random_rotate == 0:
             self.rotate_ship()
-        
-        (x,y) = self.return_size()
+
+        (x, y) = self.return_size()
         random_col = random.randint(0, 9 - y)
         random_row = random.randint(0, 9 - x)
         for node in self.nodes:
@@ -164,15 +167,13 @@ class Ship:
         """
         Method to return ship size as tuple
         """
-        #Return ship dimensions with respective size -1
-        return (self.horizontal_size -1, self.vertical_size -1)
-    
+        # Return ship dimensions with respective size -1
+        return (self.horizontal_size - 1, self.vertical_size - 1)
+
     def reset_ship(self):
-        for x in range(0 ,self.size):
+        for x in range(0, self.size):
             self.nodes[x].reset_node()
             self.nodes[x] = Node(x, 0)
         self.horizontal_size = len(self.nodes)
         self.vertical = False
         self.vertical_size = 1
-
-
